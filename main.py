581a07d163e411a895c4d35c09baada02cd85b3b
GajBorade <141010355+GajBorade@users.noreply.github.com>
2025-09-11 05:56:13 +0200

    test code for Store class functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from products import Product
+from store import Store
 
 bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
 mac = Product("MacBook Air M2", price=1450, quantity=100)
@@ -12,3 +13,18 @@ mac.show()
 
 bose.set_quantity(1000)
 bose.show()
+
+#test store.py
+print('*' * 128)
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
+               ]
+
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+for product in products:
+    product.show()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[1], 2)]))
+print(best_buy.order([(products[2], 1)]))
